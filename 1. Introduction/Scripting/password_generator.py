54f2ec48e6b2b8a0d43ce3986995edaeeaaba679
hlin <henrylin2008@yahoo.com>
2021-06-03 10:02:08 -0700

    generate a password with random module

---
+++ 1. Introduction/Scripting/password_generator.py
@@ -0,0 +1,29 @@
+import random
+
+# We begin with an empty `word_list`
+word_file = "words.txt"
+word_list = []
+
+# We fill up the word_list from the `words.txt` file
+with open(word_file, 'r') as words:
+    for line in words:
+        # remove white space and make everything lowercase
+        word = line.strip().lower()
+        # don't include words that are too long or too short
+        if 3 < len(word) < 8:
+            word_list.append(word)
+
+
+# generate_password function
+# It should return a string consisting of three random words
+# concatenated together without spaces
+def generate_password():
+    random_words = random.sample(word_list, 3)
+    password = ''
+    for word in random_words:
+        password += word
+    return password
+
+
+# Now we test the function
+print(generate_password())

49593bd45e5c63df2ef44e46d26d524052ab9ce4
hlin <henrylin2008@yahoo.com>
2021-06-05 23:47:19 -0700

    hamming distance

--- 2. Data Structures/Arrays and Linked Lists/Strings.py
+++ 2. Data Structures/Arrays and Linked Lists/Strings.py
@@ -94,8 +94,38 @@ def word_flipper(our_string):
 
 
 # test cases
-print("Pass" if ('retaw' == word_flipper('water')) else "Fail")    # Pass
-print("Pass" if ('sihT si na elpmaxe' == word_flipper('This is an example')) else "Fail")   # Pass
-print("Pass" if ('sihT si eno llams pets rof ...' == word_flipper('This is one small step for ...')) else "Fail") # Pass
+print("Pass" if ('retaw' == word_flipper('water')) else "Fail")  # Pass
+print("Pass" if ('sihT si na elpmaxe' == word_flipper('This is an example')) else "Fail")  # Pass
+print(
+    "Pass" if ('sihT si eno llams pets rof ...' == word_flipper('This is one small step for ...')) else "Fail")  # Pass
 
 
+# Exercise 4: Hamming Distance
+def hamming_distance(str1, str2):
+    """
+    Calculate the hamming distance of the two strings
+
+    Args:
+       str1(string),str2(string): Strings to be used for finding the hamming distance
+    Returns:
+       int: Hamming Distance
+    """
+
+    if len(str1) == len(str2):
+        count = 0
+
+        for char in range(len(str1)):
+            if str1[char] != str2[char]:
+                count += 1
+
+        return count
+
+    return None
+
+
+# Test cases
+print("Pass" if (10 == hamming_distance('ACTTGACCGGG', 'GATCCGGTACA')) else "Fail")
+print("Pass" if (1 == hamming_distance('shove', 'stove')) else "Fail")
+print("Pass" if (None == hamming_distance('Slot machines', 'Cash lost in me')) else "Fail")
+print("Pass" if (9 == hamming_distance('A gentleman', 'Elegant men')) else "Fail")
+print("Pass" if (2 == hamming_distance('0101010100011101', '0101010100010001')) else "Fail")

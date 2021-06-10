5c7bd7fb72b8140e9a00ebc57f9e7745f412699d
hlin <henrylin2008@yahoo.com>
2021-06-10 09:00:56 -0700

    remove, pop, insert

--- 2. Data Structures/Arrays and Linked Lists/linked_list_practice.py
+++ 2. Data Structures/Arrays and Linked Lists/linked_list_practice.py
@@ -95,3 +95,92 @@ LinkedList.search = search
 # assert linked_list.search(1).value == 1, f"list contents: {linked_list.to_list()}"
 # assert linked_list.search(4).value == 4, f"list contents: {linked_list.to_list()}"
 
+
+def remove(self, value):
+    """ Remove first occurrence of value. """
+    if self.head is None:
+        return
+
+    if self.head.value == value:
+        self.head = self.head.next
+        return
+
+    node = self.head
+    while node.next:
+        if node.next.value == value:
+            node.next = node.next.next
+            return
+        node = node.next
+
+    raise ValueError("Value not found in the list.")
+
+
+LinkedList.remove = remove
+
+# Test remove
+# linked_list.remove(1)     # [2, 1, 3, 4, 3]
+# assert linked_list.to_list() == [2, 1, 3, 4, 3], f"list contents: {linked_list.to_list()}"
+# linked_list.remove(3)     # [2, 1, 4, 3]
+# assert linked_list.to_list() == [2, 1, 4, 3], f"list contents: {linked_list.to_list()}"
+# linked_list.remove(3)     # [2, 1, 4]
+# assert linked_list.to_list() == [2, 1, 4], f"list contents: {linked_list.to_list()}"
+
+def pop(self):
+    """ Return the first node's value and remove it from the list. """
+    if self.head is None:
+        return None
+
+    node = self.head
+    self.head = self.head.next
+
+    return node.value
+
+
+LinkedList.pop = pop
+
+# Test pop
+# value = linked_list.pop()
+# print(linked_list.to_list())  # [1, 4]
+# assert value == 2, f"list contents: {linked_list.to_list()}"
+# assert linked_list.head.value == 1, f"list contents: {linked_list.to_list()}"
+
+
+def insert(self, value, pos):
+    """ Insert value at pos position in the list. If pos is larger than the
+    length of the list, append to the end of the list. """
+    if self.head is None:
+        self.head = Node(value)
+        return
+
+    if pos == 0:
+        self.prepend(value)
+        return
+
+    index = 0
+    node = self.head
+    while node.next and index <= pos:
+        if (pos - 1) == index:
+            new_node = Node(value)
+            new_node.next = node.next
+            node.next = new_node
+            return
+
+        index += 1
+        node = node.next
+    else:
+        self.append(value)
+
+
+LinkedList.insert = insert
+
+# Test insert
+# linked_list.insert(5, 0)
+# print(linked_list.to_list())  # [5, 1, 4]
+# assert linked_list.to_list() == [5, 1, 4], f"list contents: {linked_list.to_list()}"
+# linked_list.insert(2, 1)
+# print(linked_list.to_list())  # [5, 2, 1, 4]
+# assert linked_list.to_list() == [5, 2, 1, 4], f"list contents: {linked_list.to_list()}"
+# linked_list.insert(3, 6)
+# print(linked_list.to_list())  # [5, 2, 1, 4, 3]
+# assert linked_list.to_list() == [5, 2, 1, 4, 3], f"list contents: {linked_list.to_list()}"
+

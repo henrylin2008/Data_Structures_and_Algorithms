ad6998fa5e5201ba4fec1fa1ba6e0dff557c5363
hlin <henrylin2008@yahoo.com>
2021-06-14 10:22:19 -0700

    adding test cases

--- 2. Data Structures/Arrays and Linked Lists/add_one.py
+++ 2. Data Structures/Arrays and Linked Lists/add_one.py
@@ -80,3 +80,86 @@ def add_one(arr):
         position += 1
 
     return arr[position:]
+
+
+# -------------------------------------#
+# Descriptive Example 1 - Original array is [1, 2, 3]
+# -------------------------------------#
+
+'''
+FOR LOOP BEGINS
+    For i=3 , arr[2]=3 , digit=4 , borrow=0
+        BORROW COMPARISON START
+            Since borrow is 0, update arr[2] = digit =  4 and quit the FOR loop.
+            NO need to check other digits on the left of current digit
+FOR LOOP ENDS
+
+Append  [0] to the beginning of the original arr. Now arr = [0, 1, 2, 4]
+In this final updated arr, find a position from where to return the list. This position (index) =  1
+Return  [1, 2, 4]
+
+'''
+# -------------------------------------#
+# Descriptive Example 2 - Original array is [9, 9, 9]
+# -------------------------------------#
+'''
+FOR LOOP BEGINS
+    For i=  3 , arr[ 2 ] =  9 , digit =  10 , borrow =  1
+        BORROW COMPARISON START
+            Since borrow is non-zero, update arr[ 2 ] = digit % 10 =  0
+            Update output = borrow = 1
+        BORROW COMPARISON ENDS 
+
+    For i=  2 , arr[ 1 ] =  9 , digit =  10 , borrow =  1
+        BORROW COMPARISON START
+            Since borrow is non-zero, update arr[ 1 ] = digit % 10 =  0
+            Update output = borrow = 1
+        BORROW COMPARISON ENDS 
+
+    For i=  1 , arr[ 0 ] =  9 , digit =  10 , borrow =  1
+        BORROW COMPARISON START
+            Since borrow is non-zero, update arr[ 0 ] = digit % 10 =  0
+            Update output = borrow = 1
+        BORROW COMPARISON ENDS 
+
+FOR LOOP ENDS
+
+Append  [1] to the beginning of the original arr. Now arr = [1, 0, 0, 0]
+
+In this final updated arr, find a position from where to return the list. This position (index) =  0
+Return  [1, 0, 0, 0] 
+'''
+
+
+# A helper function for Test Cases
+def test_function(test_case):
+    arr = test_case[0]
+    solution = test_case[1]
+
+    output = add_one(arr)
+    for index, element in enumerate(output):
+        if element != solution[index]:
+            print("Fail")
+            return
+    print("Pass")
+
+
+# Test Case 1
+arr = [0]
+solution = [1]
+test_case = [arr, solution]
+test_function(test_case)    # Pass
+
+
+# Test Case 2
+arr = [1, 2, 3]
+solution = [1, 2, 4]
+test_case = [arr, solution]
+test_function(test_case)    # Pass
+
+
+# Test Case 3
+arr = [9, 9, 9]
+solution = [1, 0, 0, 0]
+test_case = [arr, solution]
+test_function(test_case)    # Pass

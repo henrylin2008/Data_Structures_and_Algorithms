16121422397b091a37ab71e3ce635df7a2956f8d
hlin <henrylin2008@yahoo.com>
2021-06-13 22:56:50 -0700

    adding NestedLinkedList class

--- 2. Data Structures/Arrays and Linked Lists/flatten_linked_list.py
+++ 2. Data Structures/Arrays and Linked Lists/flatten_linked_list.py
@@ -88,3 +88,17 @@ def merge(list1, list2):
             list2_elt = list2_elt.next
     return merged
 
+
+''' In a NESTED LinkedList object, each node will be a simple LinkedList in itself'''
+class NestedLinkedList(LinkedList):
+    def flatten(self):
+        return self._flatten(self.head)  # <-- self.head is a node for NestedLinkedList
+
+    '''  A recursive function '''
+    def _flatten(self, node):
+        # A termination condition
+        if node.next is None:
+            return merge(node.value, None)  # <-- First argument is a simple LinkedList
+
+        # _flatten() is calling itself untill a termination condition is achieved
+        return merge(node.value, self._flatten(node.next))  # <-- Both arguments are a simple LinkedList each

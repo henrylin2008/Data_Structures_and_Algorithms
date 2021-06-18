e6b31b6acb20121dca1b53b587a9df6e2cb6bd99
hlin <henrylin2008@yahoo.com>
2021-06-17 22:55:46 -0700

    Even after odd nodes

---
+++ 2. Data Structures/Arrays and Linked Lists/even_after_odd_nodes.py
@@ -0,0 +1,10 @@
+# Problem Statement
+# Given a linked list with integer data, arrange the elements in such a manner that all nodes with
+# even numbers are placed after odd numbers. Do not create any new nodes and avoid using any other data structure.
+# The relative order of even and odd elements must not change.
+#
+# Example:
+#
+# linked list = 1 2 3 4 5 6
+# output = 1 3 5 2 4 6
+

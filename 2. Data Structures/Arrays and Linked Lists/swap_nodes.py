ec36006130141e8f277d59147f65fe8d90303408
hlin <henrylin2008@yahoo.com>
2021-06-20 23:20:19 -0700

    swap nodes

---
+++ 2. Data Structures/Arrays and Linked Lists/swap_nodes.py
@@ -0,0 +1,23 @@
+# Problem Statement
+# Given a linked list, swap the two nodes present at position i and j, assuming 0 <= i <= j. The
+# positions are based on 0-based indexing.
+#
+# Note: You have to swap the nodes and not just the values.
+#
+# Example:
+#
+# -linked_list = 3 4 5 2 6 1 9
+# -positions = 2 5
+# -output = 3 4 1 2 6 5 9
+#
+# Explanation:
+#
+# -The node at position 3 has the value 2
+# -The node at position 4 has the value 6
+# -Swapping these nodes will result in a final order of nodes of 3 4 5 6 2 1 9
+#
+# Let's take an example to understand a simple approach -
+# Given linked list = [3, 4, 5, 2, 6, 1, 9]
+# position_one = 2
+# position_two = 5
+# Note the original order of indexes - 0, 1, 2, 3, 4, 5, 6

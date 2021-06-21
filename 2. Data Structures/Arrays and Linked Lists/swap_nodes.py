e745859c09ae507d42dd57b5647de6fc0a623146
hlin <henrylin2008@yahoo.com>
2021-06-21 10:57:38 -0700

    test function

--- 2. Data Structures/Arrays and Linked Lists/swap_nodes.py
+++ 2. Data Structures/Arrays and Linked Lists/swap_nodes.py
@@ -22,13 +22,19 @@
 # position_two = 5
 # Note the original order of indexes - 0, 1, 2, 3, 4, 5, 6
 
+class Node:
+    """LinkedListNode class to be used for this problem"""
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
 """
 :param: head- head of input linked list
 :param: `position_one` - indicates position (index) ONE
 :param: `position_two` - indicates position (index) TWO
 return: head of updated linked list with nodes swapped
 """
-
 def swap_nodes(head, position_one, position_two):
     # If both the indices are same
     if position_one == position_two:
@@ -86,3 +92,67 @@ def swap_nodes(head, position_one, position_two):
     # Swapping logic ends
 
     return new_head
+
+
+def test_function(test_case):
+    head = test_case[0]
+    left_index = test_case[1]
+    right_index = test_case[2]
+
+    left_node = None
+    right_node = None
+
+    temp = head
+    index = 0
+    try:
+        while temp is not None:
+            if index == left_index:
+                left_node = temp
+            if index == right_index:
+                right_node = temp
+                break
+            index += 1
+            temp = temp.next
+
+        updated_head = swap_nodes(head, left_index, right_index)
+
+        temp = updated_head
+        index = 0
+        pass_status = [False, False]
+
+        while temp is not None:
+            if index == left_index:
+                pass_status[0] = temp is right_node
+            if index == right_index:
+                pass_status[1] = temp is left_node
+
+            index += 1
+            temp = temp.next
+
+        if pass_status[0] and pass_status[1]:
+            print("Pass")
+        else:
+            print("Fail")
+        return updated_head
+    except Exception as e:
+        print("Fail")
+
+
+# helper functions for testing purpose
+def create_linked_list(arr):
+    if len(arr) == 0:
+        return None
+    head = Node(arr[0])
+    tail = head
+    for data in arr[1:]:
+        tail.next = Node(data)
+        tail = tail.next
+    return head
+
+
+def print_linked_list(head):
+    while head:
+        print(head.data, end=" ")
+        head = head.next
+    print()
+

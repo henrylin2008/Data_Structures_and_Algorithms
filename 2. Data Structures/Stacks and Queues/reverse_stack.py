1984df1c3d243bf959dfb0ecd16c5117f1226e9e
hlin <henrylin2008@yahoo.com>
2021-06-26 23:38:07 -0700

    added test function and test cases

--- 2. Data Structures/Stacks and Queues/reverse_stack.py
+++ 2. Data Structures/Stacks and Queues/reverse_stack.py
@@ -66,3 +66,27 @@ def _reverse_stack_recursion(stack, holder_stack):
     popped_element = holder_stack.pop()
     _reverse_stack_recursion(stack, holder_stack)
     stack.push(popped_element)
+
+
+def test_function(test_case):
+    stack = Stack()
+    for num in test_case:
+        stack.push(num)
+
+    reverse_stack(stack)
+    index = 0
+    while not stack.is_empty():
+        popped = stack.pop()
+        if popped != test_case[index]:
+            print("Fail")
+            return
+        else:
+            index += 1
+    print("Pass")
+
+# test_case_1 = [1, 2, 3, 4]
+# test_function(test_case_1)  # Pass
+
+
+# test_case_2 = [1]
+# test_function(test_case_2)    # Pass

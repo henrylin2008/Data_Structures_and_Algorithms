7c8e4d55aa343f901534a38bf188c0d85c77c3f8
hlin <henrylin2008@yahoo.com>
2021-06-28 22:39:36 -0700

    implement a queue using an array

---
+++ 2. Data Structures/Stacks and Queues/build_a_queue_using_an_array.py
@@ -0,0 +1,18 @@
+# Implement a queue using an array
+
+# Functionality
+# Once implemented, our queue will need to have the following functionality:
+#
+# enqueue - adds data to the back of the queue
+# dequeue - removes data from the front of the queue
+# front - returns the element at the front of the queue
+# size - returns the number of elements present in the queue
+# is_empty - returns True if there are no elements in the queue, and False otherwise
+# _handle_full_capacity - increases the capacity of the array, for cases in which the queue would otherwise overflow
+class Queue:
+
+    def __init__(self, initial_size=10):
+        self.arr = [0 for _ in range(initial_size)]
+        self.next_index = 0
+        self.front_index = -1
+        self.queue_size = 0

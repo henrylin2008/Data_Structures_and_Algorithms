8916f5217f4998f49a7acccf14fd1e5e9d5d6264
hlin <henrylin2008@yahoo.com>
2021-06-29 22:35:18 -0700

    added testes

--- 2. Data Structures/Stacks and Queues/build_a_queue_using_a_linked_list.py
+++ 2. Data Structures/Stacks and Queues/build_a_queue_using_a_linked_list.py
@@ -38,8 +38,8 @@ class Queue:
     def dequeue(self):
         if self.is_empty():
             return None
-        value = self.head.value          # copy the value to a local variable
-        self.head = self.head.next       # shift the head (i.e., the front of the queue)
+        value = self.head.value  # copy the value to a local variable
+        self.head = self.head.next  # shift the head (i.e., the front of the queue)
         self.num_elements -= 1
         return value
 
@@ -50,3 +50,23 @@ class Queue:
         return self.num_elements == 0
 
 
+# tests:
+# Setup
+# q = Queue()
+# q.enqueue(1)
+# q.enqueue(2)
+# q.enqueue(3)
+#
+# # Test size
+# print("Pass" if (q.size() == 3) else "Fail")    # Pass
+#
+# # Test dequeue
+# print("Pass" if (q.dequeue() == 1) else "Fail")     # Pass
+#
+# # Test enqueue
+# q.enqueue(4)
+# print("Pass" if (q.dequeue() == 2) else "Fail")     # Pass
+# print("Pass" if (q.dequeue() == 3) else "Fail")     # Pass
+# print("Pass" if (q.dequeue() == 4) else "Fail")     # Pass
+# q.enqueue(5)
+# print("Pass" if (q.size() == 1) else "Fail")        # Pass

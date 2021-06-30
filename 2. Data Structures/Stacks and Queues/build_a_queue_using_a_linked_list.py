0e9905c9063fac633a48675aa65c397c2c3adf4e
hlin <henrylin2008@yahoo.com>
2021-06-29 22:23:17 -0700

    build a queue using a linked list

---
+++ 2. Data Structures/Stacks and Queues/build_a_queue_using_a_linked_list.py
@@ -0,0 +1,37 @@
+# Build a queue using a linked list
+#
+# It's good to try implementing the same data structures in multiple ways. This helps you to better understand the
+# abstract concepts behind the data structure, separate from the details of their implementation—and it also helps
+# you develop a habit of comparing pros and cons of different implementations.
+#
+# With both stack and queues, we saw that trying to use arrays introduced some concerns regarding the time
+# complexity, particularly when the initial array size isn't large enough and we need to expand the array in order to
+# add more items.
+#
+# With our stack implementation, we saw that linked lists provided a way around this issue—and exactly the same thing
+# is true with queues.
+
+class Node:
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Queue:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.num_elements = 0
+
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = self.head
+        else:
+            self.tail.next = new_node  # add data to the next attribute of the tail (i.e. the end of the queue)
+            self.tail = self.tail.next  # shift the tail (i.e., the back of the queue)
+        self.num_elements += 1
+

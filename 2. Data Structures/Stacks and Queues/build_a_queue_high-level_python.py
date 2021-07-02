e8de6bd357da57ed389d138d42385d724160ef93
hlin <henrylin2008@yahoo.com>
2021-07-01 22:58:37 -0700

    added test cases

--- 2. Data Structures/Stacks and Queues/build_a_queue_high-level_python.py
+++ 2. Data Structures/Stacks and Queues/build_a_queue_high-level_python.py
@@ -26,6 +26,24 @@ class Queue:
 
     def dequeue(self):
         return self.storage.pop(0)
-
-
-
+#
+#
+# Test cases
+# q = Queue()
+# q.enqueue(1)
+# q.enqueue(2)
+# q.enqueue(3)
+#
+# # Test size
+# print("Pass" if (q.size() == 3) else "Fail")  # Pass
+#
+# # Test dequeue
+# print("Pass" if (q.dequeue() == 1) else "Fail")   # Pass
+#
+# # Test enqueue
+# q.enqueue(4)
+# print("Pass" if (q.dequeue() == 2) else "Fail")   # Pass
+# print("Pass" if (q.dequeue() == 3) else "Fail")   # Pass
+# print("Pass" if (q.dequeue() == 4) else "Fail")   # Pass
+# q.enqueue(5)
+# print("Pass" if (q.size() == 1) else "Fail")      # Pass

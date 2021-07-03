41b896bf04477b4480d6e94bfceab3138a4634fc
hlin <henrylin2008@yahoo.com>
2021-07-03 12:55:10 -0700

    reverse a string

---
+++ 2. Data Structures/Recursion/reverse_a_string.py
@@ -0,0 +1,30 @@
+# Reversing a string
+# Each recursive call to the `reverse_string()` function will create
+# a new set of local variables - first_char, the_rest, sub_string, and reversed_substring.
+# Therefore, the space complexity of a recursive function would always be proportional to the
+# maximum depth of recursion stack.
+# The time complexity for this function will be  O(k*n), where k is a constant and n is the
+# number of characters in the string (depth of recursion stack).
+def reverse_string(input_string):
+    # (Recursion) Termination condition / Base condition
+    if len(input_string) == 0:
+        return ""
+
+    else:
+        first_char = input_string[0]
+
+        '''
+        The `slice()` function can accept upto the following three arguments.
+        - start: [OPTIONAL] starting index. Default value is 0.
+        - stop: ending index (exclusive)
+        - step_size: [OPTIONAL] the increment size. Default value is 1.
+
+        The return type of `slice()` function is an object of class 'slice'. 
+        '''
+        the_rest = slice(1, None)  # `the_rest` is an object of type 'slice' class
+        sub_string = input_string[the_rest]  # convert the `slice` object into a list
+
+        # Recursive call
+        reversed_substring = reverse_string(sub_string)
+        return reversed_substring + first_char
+

e8eca73ce89cbe6c52280363ff61e58b2b8d5f4e
hlin <henrylin2008@yahoo.com>
2021-07-04 23:12:49 -0700

    test function and test cases

--- 2. Data Structures/Recursion/list_permutation.py
+++ 2. Data Structures/Recursion/list_permutation.py
@@ -62,3 +62,36 @@ def permute(input_list):
                 final_compound_list.append(b_list)
 
     return final_compound_list
+
+
+def check_output(output, expected_output):
+    """
+    Return True if output and expected_output
+    contains the same lists, False otherwise.
+
+    Note that the ordering of the list is not important.
+
+    Examples:
+        check_output([ [0, 1], [1, 0] ] ], [ [1, 0], [0, 1] ]) returns True
+
+    Args:
+        output(list): list of list
+        expected_output(list): list of list
+
+    Returns:
+        bool
+    """
+    o = copy.deepcopy(output)  # so that we don't mutate input
+    e = copy.deepcopy(expected_output)  # so that we don't mutate input
+
+    o.sort()
+    e.sort()
+    return o == e
+
+
+# print("Pass" if (check_output(permute([]), [[]])) else "Fail")  # Pass
+# print("Pass" if (check_output(permute([0]), [[0]])) else "Fail")  # Pass
+# print("Pass" if (check_output(permute([0, 1]), [[0, 1], [1, 0]])) else "Fail")    # Pass
+# print("Pass" if (
+#     check_output(permute([0, 1, 2]), [[0, 1, 2], [0, 2, 1], [1, 0, 2], [1, 2, 0], [2, 0, 1], [2, 1, 0]])) else "Fail")
+#     # Pass

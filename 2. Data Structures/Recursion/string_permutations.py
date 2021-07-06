247809172933b5e7b73fd7daaeed225cea9007de
hlin <henrylin2008@yahoo.com>
2021-07-05 23:40:50 -0700

    added solution

--- 2. Data Structures/Recursion/string_permutations.py
+++ 2. Data Structures/Recursion/string_permutations.py
@@ -31,4 +31,26 @@ def permutations(string):
     :param: input string
     Return - list of all permutations of the input string
     """
+    return return_permutations(string, 0)
+
+def return_permutations(string, index):
+    # output to be returned
+    output = list()
+
+    # Termination / Base condition
+    if index >= len(string):
+        return [""]
+
+    # Recursive function call
+    small_output = return_permutations(string, index + 1)
+    # Pick a character
+    current_char = string[index]
+    # Iterate over each sub-string available in the list returned from previous call
+    for subString in small_output:
+        # place the current character at different indices of the sub-string
+        for index in range(len(small_output[0]) + 1):
+            # Make use of the sub-string of previous output, to create a new sub-string.
+            new_sub_string = subString[0: index] + current_char + subString[index:]
+            output.append(new_sub_string)
+    return output
 

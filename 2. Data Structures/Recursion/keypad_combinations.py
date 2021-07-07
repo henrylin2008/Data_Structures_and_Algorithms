4104a8d80578f31f216986e3427b170b4aca8c5b
hlin <henrylin2008@yahoo.com>
2021-07-06 23:17:59 -0700

    added solution

--- 2. Data Structures/Recursion/keypad_combinations.py
+++ 2. Data Structures/Recursion/keypad_combinations.py
@@ -15,3 +15,60 @@
 # Given an integer num, find out all the possible strings that can be made using digits of input num. Return these
 # strings in a list. The order of strings in the list does not matter. However, as stated earlier, the order of
 # letters in a particular string matters.
+
+def get_characters(num):
+    if num == 2:
+        return "abc"
+    elif num == 3:
+        return "def"
+    elif num == 4:
+        return "ghi"
+    elif num == 5:
+        return "jkl"
+    elif num == 6:
+        return "mno"
+    elif num == 7:
+        return "pqrs"
+    elif num == 8:
+        return "tuv"
+    elif num == 9:
+        return "wxyz"
+    else:
+        return ""
+
+
+# Recursive Solution
+def keypad(num):
+    # Base case
+    if num <= 1:
+        return [""]
+
+    # If `num` is single digit, get the LIST having one element - the associated string
+    elif 1 < num <= 9:
+        return list(get_characters(num))
+
+    # Otherwise `num` >= 10. Find the unit's (last) digits of `num`
+    last_digit = num % 10
+
+    '''Step 1'''
+    # Recursive call to the same function with “floor” of the `num//10`
+    small_output = keypad(num // 10)  # returns a LIST of strings; returns from the previous (recursive) output
+
+    '''Step 2'''
+    # Get the associated string for the `last_digit`
+    keypad_string = get_characters(last_digit)  # returns a string
+
+    '''Permute the characters of result obtained from Step 1 and Step 2'''
+    output = list()
+
+    '''
+    The Idea:
+    Each character of keypad_string must be appended to the 
+    end of each string available in the small_output
+    '''
+    for character in keypad_string:
+        for item in small_output:
+            new_item = item + character
+            output.append(new_item)
+
+    return output  # returns a LIST of strings

3249798977b0b261d272df1608dafd93b6161b87
hlin <henrylin2008@yahoo.com>
2021-07-07 08:22:35 -0700

    added test function and test cases

--- 2. Data Structures/Recursion/deep_reverse.py
+++ 2. Data Structures/Recursion/deep_reverse.py
@@ -42,3 +42,36 @@ def deep_reverse(arr):
         reversed_items.append(item)
 
     return reversed_items
+
+
+def test_function(test_case):
+    arr = test_case[0]
+    solution = test_case[1]
+
+    output = deep_reverse(arr)
+    if output == solution:
+        print("Pass")
+    else:
+        print("False")
+
+
+# Test cases
+# arr = [1, 2, 3, 4, 5]
+# solution = [5, 4, 3, 2, 1]
+# test_case = [arr, solution]
+# test_function(test_case)  # Pass
+#
+# arr = [1, 2, [3, 4, 5], 4, 5]
+# solution = [5, 4, [5, 4, 3], 2, 1]
+# test_case = [arr, solution]
+# test_function(test_case)  # Pass
+#
+# arr = [1, [2, 3, [4, [5, 6]]]]
+# solution = [[[[6, 5], 4], 3, 2], 1]
+# test_case = [arr, solution]
+# test_function(test_case)  # Pass
+#
+# arr = [1, [2, 3], 4, [5, 6]]
+# solution = [[6, 5], 4, [3, 2], 1]
+# test_case = [arr, solution]
+# test_function(test_case)  # Pass

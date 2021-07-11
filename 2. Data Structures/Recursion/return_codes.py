6018ba74c3f29c1c28dab0862b66e3e240030f6b
hlin <henrylin2008@yahoo.com>
2021-07-11 14:38:14 -0700

    added solution

--- 2. Data Structures/Recursion/return_codes.py
+++ 2. Data Structures/Recursion/return_codes.py
@@ -20,3 +20,47 @@
 # Return the codes in a list. The order of codes in the list is not important.
 #
 # Note: you can assume that the input number will not contain any 0s
+
+
+def get_alphabet(number):
+    """
+    Helper function to figure out alphabet of a particular number
+    Remember:
+        * ASCII for lower case 'a' = 97
+        * chr(num) returns ASCII character for a number e.g. chr(65) ==> 'A'
+    """
+    return chr(number + 96)
+
+
+def all_codes(number):
+    if number == 0:
+        return [""]
+
+    # calculation for two right-most digits e.g. if number = 1123, this calculation is meant for 23
+    remainder = number % 100
+    output_100 = list()
+    # get the letter that's >= 10 and <= 26, ex: j(10) <--> z(26)
+    if remainder <= 26 and number > 9:
+
+        # get all codes for the remaining number
+        output_100 = all_codes(number // 100)
+        alphabet = get_alphabet(remainder)
+
+        for index, element in enumerate(output_100):
+            output_100[index] = element + alphabet
+
+    # calculation for right-most digit e.g. if number = 1123, this calculation is meant for 3
+    remainder = number % 10
+
+    # get all codes for the remaining number
+    output_10 = all_codes(number // 10)
+    alphabet = get_alphabet(remainder)  # get the alphabet representation of the remainder
+
+    for index, element in enumerate(output_10):
+        output_10[index] = element + alphabet
+
+    output = list()
+    output.extend(output_100)
+    output.extend(output_10)
+
+    return output

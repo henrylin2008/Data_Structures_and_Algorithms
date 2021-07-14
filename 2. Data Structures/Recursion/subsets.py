f675640b779d60eb642e7f9d3ec2ad4b23c3e5fc
hlin <henrylin2008@yahoo.com>
2021-07-13 23:31:44 -0700

    added solution

--- 2. Data Structures/Recursion/subsets.py
+++ 2. Data Structures/Recursion/subsets.py
@@ -28,3 +28,26 @@
 #            [9, 15],
 #            [9, 12],
 #            [9, 12, 15]]
+
+
+def subsets(arr):
+    return return_subsets(arr, 0)
+
+def return_subsets(arr, index):
+    if index >= len(arr):
+        return [[]]
+
+    small_output = return_subsets(arr, index + 1)
+
+    output = list()
+    # append existing subsets
+    for element in small_output:
+        output.append(element)
+
+    # add current elements to existing subsets and add them to the output
+    for element in small_output:
+        current = list()
+        current.append(arr[index])
+        current.extend(element)
+        output.append(current)
+    return output

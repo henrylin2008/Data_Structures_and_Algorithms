4f84cd88f147449219e9eb46bbf5d8735c645584
hlin <henrylin2008@yahoo.com>
2021-07-14 23:56:52 -0700

    adding test cases

--- 2. Data Structures/Recursion/subsets.py
+++ 2. Data Structures/Recursion/subsets.py
@@ -67,3 +67,46 @@ def test_function(test_case):
     else:
         print("Fail")
 
+
+# Test Case 1
+# arr = [9]
+# solution = [[], [9]]
+#
+# test_case = [arr, solution]
+# test_function(test_case)  # Pass
+
+# Test 2
+# arr = [5, 7]
+# solution = [[], [7], [5], [5, 7]]
+# test_case = [arr, solution]
+# test_function(test_case)  # Pass
+
+# Test 3
+# arr = [9, 12, 15]
+# solution = [[], [15], [12], [12, 15], [9], [9, 15], [9, 12], [9, 12, 15]]
+#
+# test_case = [arr, solution]
+# test_function(test_case)  # Pass
+
+
+# Test 4
+# arr = [9, 8, 9, 8]
+# solution = [[],
+# [8],
+# [9],
+# [9, 8],
+# [8],
+# [8, 8],
+# [8, 9],
+# [8, 9, 8],
+# [9],
+# [9, 8],
+# [9, 9],
+# [9, 9, 8],
+# [9, 8],
+# [9, 8, 8],
+# [9, 8, 9],
+# [9, 8, 9, 8]]
+#
+# test_case = [arr, solution]
+# test_function(test_case)  # Pass

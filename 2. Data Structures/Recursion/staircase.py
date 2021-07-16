ae598c52d1ec062d79bc7e629ffbef84d3d40544
hlin <henrylin2008@yahoo.com>
2021-07-16 00:01:25 -0700

    test function and test cases

--- 2. Data Structures/Recursion/staircase.py
+++ 2. Data Structures/Recursion/staircase.py
@@ -31,3 +31,31 @@ def staircase(n):
 
     return staircase(n-1) + staircase(n-2) + staircase(n-3)
 
+
+def test_function(test_case):
+    n = test_case[0]
+    solution = test_case[1]
+    output = staircase(n)
+    if output == solution:
+        print("Pass")
+    else:
+        print("Fail")
+
+
+# Test case 1
+# n = 3
+# solution = 4
+# test_case = [n, solution]
+# test_function(test_case)    # Pass
+
+# Test case 2
+# n = 4
+# solution = 7
+# test_case = [n, solution]
+# test_function(test_case)    # Pass
+
+# Test case 3
+# n = 7
+# solution = 44
+# test_case = [n, solution]
+# test_function(test_case)    # Pass

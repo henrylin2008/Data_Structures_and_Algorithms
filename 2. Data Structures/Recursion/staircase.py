c33e342137dad20d613de9e20765e7920f75cf25
hlin <henrylin2008@yahoo.com>
2021-07-15 23:09:41 -0700

    staircase

---
+++ 2. Data Structures/Recursion/staircase.py
@@ -0,0 +1,15 @@
+# Problem Statement
+
+# Suppose there is a staircase that you can climb in either 1 step, 2 steps, or 3 steps. In how many possible ways
+# can you climb the staircase if the staircase has n steps? Write a recursive function to solve the problem.
+#
+# Example:
+#
+# n == 1 then answer = 1
+# n == 3 then answer = 4
+# The output is 4 because there are four ways we can climb the staircase:
+#   1 step + 1 step + 1 step
+#   1 step + 2 steps
+#   2 steps + 1 step
+#   3 steps
+# n == 5 then answer = 13

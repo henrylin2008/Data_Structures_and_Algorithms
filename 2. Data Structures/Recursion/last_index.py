b1c705eb45c2d13e0184b1e47161f200510df37f
hlin <henrylin2008@yahoo.com>
2021-07-16 21:59:00 -0700

    adding solution

--- 2. Data Structures/Recursion/last_index.py
+++ 2. Data Structures/Recursion/last_index.py
@@ -7,3 +7,19 @@
 # For arr = [1, 2, 5, 5, 1, 2, 5, 4] and target = 5, output = 6
 #
 # For arr = [1, 2, 5, 5, 1, 2, 5, 4] and target = 7, output = -1
+
+def last_index(arr, target):
+    # we start looking from the last index
+    return last_index_arr(arr, target, len(arr) - 1)
+
+
+def last_index_arr(arr, target, index):
+    if index < 0:
+        return -1
+
+    # check if target is found
+    if arr[index] == target:
+        return index
+
+    # else make a recursive call to the rest of the array
+    return last_index_arr(arr, target, index - 1)

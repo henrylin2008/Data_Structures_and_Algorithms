9dbe556e1938b77899029ac74dfe58180788af49
hlin <henrylin2008@yahoo.com>
2021-07-17 08:05:58 -0700

    Depth First Search

---
+++ 2. Data Structures/Trees/dfs.py
@@ -0,0 +1,86 @@
+# Depth First Search
+# Pre-order traversal
+#               apple
+#             /       \
+#          banana     cherry
+#          /
+#        dates
+# Pre-order: apple, banana, dates, cherry
+# this code makes the tree that we'll traverse
+
+class Node(object):
+
+    def __init__(self, value=None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def set_value(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
+
+    def set_left_child(self, left):
+        self.left = left
+
+    def set_right_child(self, right):
+        self.right = right
+
+    def get_left_child(self):
+        return self.left
+
+    def get_right_child(self):
+        return self.right
+
+    def has_left_child(self):
+        return self.left is not None
+
+    def has_right_child(self):
+        return self.right is not None
+
+    # define __repr_ to decide what a print statement displays for a Node object
+    def __repr__(self):
+        return f"Node({self.get_value()})"
+
+    def __str__(self):
+        return f"Node({self.get_value()})"
+
+
+class Tree:
+    def __init__(self, value=None):
+        self.root = Node(value)
+
+    def get_root(self):
+        return self.root
+
+
+# Define a stack to help keep track of the tree nodes
+class Stack:
+    def __init__(self):
+        self.list = list()
+
+    def push(self, value):
+        self.list.append(value)
+
+    def pop(self):
+        return self.list.pop()
+
+    def top(self):
+        if len(self.list) > 0:
+            return self.list[-1]
+        else:
+            return None
+
+    def is_empty(self):
+        return len(self.list) == 0
+
+    def __repr__(self):  # print format
+        if len(self.list) > 0:
+            s = "<top of stack>\n_________________\n"
+            s += "\n_________________\n".join([str(item) for item in self.list[::-1]])
+            s += "\n_________________\n<bottom of stack>"
+            return s
+
+        else:
+            return "<stack is empty>"

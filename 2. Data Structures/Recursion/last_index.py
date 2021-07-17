4a9567e4917499c451a3d3605732cfaaf3198da2
hlin <henrylin2008@yahoo.com>
2021-07-16 22:00:21 -0700

    adding test function and test cases

--- 2. Data Structures/Recursion/last_index.py
+++ 2. Data Structures/Recursion/last_index.py
@@ -23,3 +23,50 @@ def last_index_arr(arr, target, index):
 
     # else make a recursive call to the rest of the array
     return last_index_arr(arr, target, index - 1)
+
+
+def test_function(test_case):
+    arr = test_case[0]
+    target = test_case[1]
+    solution = test_case[2]
+    output = last_index(arr, target)
+    if output == solution:
+        print("Pass")
+    else:
+        print("FAIL: Expected", solution, ", but you've got:", output)
+
+
+# Test case 1
+# arr = [1, 2, 5, 5, 4]
+# target = 5
+# solution = 3
+#
+# test_case = [arr, target, solution]
+# test_function(test_case)  # Pass
+
+
+# Test case 2
+# arr = [1, 2, 5, 5, 4]
+# target = 7
+# solution = -1
+#
+# test_case = [arr, target, solution]
+# test_function(test_case) # Pass
+
+
+# Test case 3
+# arr = [91, 19, 3, 8, 9]
+# target = 91
+# solution = 0
+#
+# test_case = [arr, target, solution]
+# test_function(test_case) # Pass
+
+
+# Test case 4
+# arr = [1, 1, 1, 1, 1, 1]
+# target = 1
+# solution = 5
+#
+# test_case = [arr, target, solution]
+# test_function(test_case) # Pass

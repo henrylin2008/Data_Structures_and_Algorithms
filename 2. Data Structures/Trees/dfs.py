2bc4f720ae38933accf9336a688be00a23e9f915
hlin <henrylin2008@yahoo.com>
2021-07-17 10:16:20 -0700

    DFS Pre order with Stack

--- 2. Data Structures/Trees/dfs.py
+++ 2. Data Structures/Trees/dfs.py
@@ -84,3 +84,107 @@ class Stack:
 
         else:
             return "<stack is empty>"
+
+
+# check Stack
+# stack = Stack()
+# stack.push("apple")
+# stack.push("banana")
+# stack.push("cherry")
+# stack.push("dates")
+# print(stack.pop())
+# print("\n")
+# print(stack)  # dates
+
+# Output:
+# dates
+#
+# <top of stack>
+# _________________
+# cherry
+# _________________
+# banana
+# _________________
+# apple
+# _________________
+# <bottom of stack>
+
+# State class to keep track if the node has been visited
+class State(object):
+    def __init__(self, node):
+        self.node = node
+        self.visited_left = False
+        self.visited_right = False
+
+    def get_node(self):
+        return self.node
+
+    def get_visited_left(self):
+        return self.visited_left
+
+    def get_visited_right(self):
+        return self.visited_right
+
+    def set_visited_left(self):
+        self.visited_left = True
+
+    def set_visited_right(self):
+        self.visited_right = True
+
+    def __repr__(self):
+        s = f"""{self.node}
+visited_left: {self.visited_left}
+visited_right: {self.visited_right}
+        """
+        return s
+
+# Pre order with a stack: keep going down the left node/s until no more child node, then going back up a level to
+# the parent node and visit the right node, recursive check the left and right nodes, until there's no more child
+# nodes.
+def pre_order_with_stack(tree, debug_mode=False):
+    visit_order = list()
+    stack = Stack()
+    node = tree.get_root()
+    visit_order.append(node.get_value())
+    state = State(node)
+    stack.push(state)
+    count = 0
+    while node:
+        if debug_mode:
+            print(f"""
+loop count: {count}
+current node: {node}
+stack:
+{stack}
+            """)
+        count += 1
+        if node.has_left_child() and not state.get_visited_left():
+            state.set_visited_left()
+            node = node.get_left_child()
+            visit_order.append(node.get_value())
+            state = State(node)
+            stack.push(state)
+
+        elif node.has_right_child() and not state.get_visited_right():
+            state.set_visited_right()
+            node = node.get_right_child()
+            visit_order.append(node.get_value())
+            state = State(node)
+
+        else:
+            stack.pop()
+            if not stack.is_empty():
+                state = stack.top()
+                node = state.get_node()
+            else:
+                node = None
+
+    if debug_mode:
+        print(f"""
+loop count: {count}
+current node: {node}
+stack:
+{stack}
+            """)
+    return visit_order
+

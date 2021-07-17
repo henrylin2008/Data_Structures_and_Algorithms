02d97e35faffec3d95e33c3e533676ebf99f3214
hlin <henrylin2008@yahoo.com>
2021-07-16 21:54:58 -0700

    last index

---
+++ 2. Data Structures/Recursion/last_index.py
@@ -0,0 +1,9 @@
+# Problem statement
+# Given an array arr and a target element target, find the last index of occurrence of target in arr
+# using recursion. If target is not present in arr, return -1.
+#
+# For example:
+#
+# For arr = [1, 2, 5, 5, 1, 2, 5, 4] and target = 5, output = 6
+#
+# For arr = [1, 2, 5, 5, 1, 2, 5, 4] and target = 7, output = -1

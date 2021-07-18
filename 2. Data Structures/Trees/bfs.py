c6812f9c88ed2f49a9169ac09d68f3ba0ad7fe75
hlin <henrylin2008@yahoo.com>
2021-07-18 08:30:29 -0700

    adding print function

--- 2. Data Structures/Trees/bfs.py
+++ 2. Data Structures/Trees/bfs.py
@@ -123,3 +123,50 @@ def bfs(tree):
     return visit_order      # [Node(apple), Node(banana), Node(cherry), Node(dates)]
 
 
+# Define the print function for the Tree class. Nodes on the same level are printed on the same line.
+#
+# For example, the tree we've been using would print out like this:
+#
+# Node(apple)
+# Node(banana) | Node(cherry)
+# Node(dates) | <empty> | <empty> | <empty>
+# <empty> | <empty>
+
+class Tree:
+    def __init__(self, value=None):
+        self.root = Node(value)
+
+    def get_root(self):
+        return self.root
+
+    def __repr__(self):
+        level = 0
+        q = Queue()
+        visit_order = list()
+        node = self.get_root()
+        q.enq((node, level))
+        while len(q) > 0:
+            node, level = q.deq()
+            if node is None:
+                visit_order.append(("<empty>", level))
+                continue
+            visit_order.append((node, level))
+            if node.has_left_child():
+                q.enq((node.get_left_child(), level + 1))
+            else:
+                q.enq((None, level + 1))
+            if node.has_right_child():
+                q.enq((node.get_right_child(), level+1))
+            else:
+                q.enq((None, level + 1))
+
+        s = "Tree\n"
+        previous_level = -1
+        for i in range(len(visit_order)):
+            node, level = visit_order[i]
+            if level == previous_level:  # if level is the same
+                s += " | " + str(node)   # keep it on the same row
+            else:
+                s += "\n " + str(node)   # add new line
+                previous_level = level
+        return s

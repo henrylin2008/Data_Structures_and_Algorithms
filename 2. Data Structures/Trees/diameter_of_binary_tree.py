cfed9fae5a08ee4f568962ec3d5d5bd13355c85d
hlin <henrylin2008@yahoo.com>
2021-07-20 00:04:46 -0700

    test function and test cases

--- 2. Data Structures/Trees/diameter_of_binary_tree.py
+++ 2. Data Structures/Trees/diameter_of_binary_tree.py
@@ -95,3 +95,36 @@ def convert_arr_to_binary_tree(arr):
             queue.put(right_node)
     return root
 
+
+def test_function(test_case):
+    arr = test_case[0]
+    solution = test_case[1]
+    root = convert_arr_to_binary_tree(arr)
+    output = diameter_of_binary_tree(root)
+    print(output)
+    if output == solution:
+        print("Pass")
+    else:
+        print("Fail")
+
+
+# Test case 1
+# arr = [1, 2, 3, 4, 5, None, None, None, None, None, None]
+# solution = 3
+#
+# test_case = [arr, solution]
+# test_function(test_case)  # 3, Pass
+
+# Test case 2
+# arr = [1, 2, 3, 4, None, 5, None, None, None, None, None]
+# solution = 4
+#
+# test_case = [arr, solution]
+# test_function(test_case)  # 4, Pass
+
+# Test case 3
+# arr = [1, 2, 3, None, None, 4, 5, 6, None, 7, 8, 9, 10, None, None, None, None, None, None, 11, None, None, None]
+# solution = 6
+#
+# test_case = [arr, solution]
+# test_function(test_case)  # 6, Pass

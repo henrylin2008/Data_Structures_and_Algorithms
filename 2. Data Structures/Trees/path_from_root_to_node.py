6e7f38e5d55424ac594f321163d750c6c1b30a66
hlin <henrylin2008@yahoo.com>
2021-08-14 09:03:18 -0700

    path from the root to the node

---
+++ 2. Data Structures/Trees/path_from_root_to_node.py
@@ -0,0 +1,3 @@
+# Problem Statement
+# Given the root of a Binary Tree and a data value representing a node, return the path from the root to that node in
+# the form of a list. You can assume that the binary tree has nodes with unique values.

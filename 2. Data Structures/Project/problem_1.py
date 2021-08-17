25977233035e7b6797ab7d8942ce1a1e31810aa2
hlin <henrylin2008@yahoo.com>
2021-08-16 23:57:03 -0700

    adding solution

--- 2. Data Structures/Project/problem_1.py
+++ 2. Data Structures/Project/problem_1.py
@@ -25,61 +25,72 @@
 # For the current problem, you can consider the size of cache = 5.
 #
 # Here is some boiler plate code and some example test cases to get you started on this problem:
-class DoubleNode:
+class DoubleLinkedNode:
     def __init__(self, value):
         self.value = value
         self.next = None
         self.previous = None
 
-class DoublyLinkedList:
-    def __init__(self):
-        self.head = None        # Least recently used - tail of the DoubleLinkedList
-        self.tail = None        # Most recently used - head of
-
-    # def append(self, value):
-    #     if self.head is None:
-    #         self.head = DoubleNode(value)
-    #         self.tail = self.head
-    #         return
-    #
-    #     # Move to the tail (the last node)
-    #     self.tail.next = DoubleNode(value)
-    #     self.tail.next.previous = self.tail
-    #     self.tail = self.tail.next
-    #     return
 
 class LRU_Cache(object):
 
     def __init__(self, capacity):
         # Initialize class variables
-        self.cache = {}             # using a hashmap to store key and value
         self.capacity = capacity
-        self.head = None
-        self.tail = None
+        self.cache = {}             # using a hashmap to store key and value
+        self.current_size = 0
+        self.list_head = None
+        self.list_tail = None
 
+    # O(1) time | O(1) space
     def get(self, key):
         # Retrieve item from provided key. Return -1 if nonexistent.
+        if key in self.cache:
+            node = self.cache[key]
+            self.remove_node(node)
+            self.set_head(node)
+            return node.value
         return -1
 
+    # O(1) time | O(n) space
     def set(self, key, value):
         # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item.
-        return -1
+        if self.current_size == self.capacity:
+            self.remove_LRU()
+        new_node = DoubleLinkedNode(value)
+        self.cache[key] = new_node
+        self.set_head(new_node)
+        self.current_size += 1
+
+    def remove_LRU(self):
+        node = self.list_tail
+        del self.cache[node.value]
+        self.remove_node(node)
+        self.current_size -= 1
+
+    def remove_node(self, node):
+        if node == self.list_head:  # case when the node is the head of the linked list
+            self.list_head = self.list_head.previous
+            self.list_head.next = None
+        elif node == self.list_tail:    # case when the node is the tail of the linked list
+            self.list_tail.next.previous = None
+            self.list_tail = self.list_tail.next
+        elif self.current_size == 1:
+            self.list_head = None
+            self.list_tail = None
+        else:
+            node.previous.next = node.next
+            node.next.previous = node.previous
+
+    def set_head(self, node):
+        if self.list_head is None:
+            self.list_head = node
+            self.list_tail = node
+        else:
+            self.list_head.next = node
+            node.previous = self.list_head
+            if self.list_head.previous is None:
+                self.list_head = self.list_head
+            self.list_head = node
 
 
-
-# our_cache = LRU_Cache(5)
-#
-# our_cache.set(1, 1)
-# our_cache.set(2, 2)
-# our_cache.set(3, 3)
-# our_cache.set(4, 4)
-#
-#
-# our_cache.get(1)       # returns 1
-# our_cache.get(2)       # returns 2
-# our_cache.get(9)      # returns -1 because 9 is not present in the cache
-#
-# our_cache.set(5, 5)
-# our_cache.set(6, 6)
-#
-# our_cache.get(3)      # returns -1 because the cache reached it's capacity and 3 was the least recently used entry

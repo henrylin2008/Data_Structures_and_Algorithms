b551d702c5c22402d9f732cc1ce988e2f0253743
hlin <henrylin2008@yahoo.com>
2021-08-19 23:15:15 -0700

    find files method

--- 2. Data Structures/Project/problem_2.py
+++ 2. Data Structures/Project/problem_2.py
@@ -37,3 +37,26 @@
 #
 # Here is some code for the function to get you started:
 
+import os
+
+# Let us print the files in the directory in which you are running this script
+# print(os.listdir("."))
+
+
+def find_files(suffix, path):
+    """
+    Find all files beneath path with file name suffix.
+
+    Note that a path may contain further subdirectories
+    and those subdirectories may also contain further subdirectories.
+
+    There are no limit to the depth of the subdirectories can be.
+
+    Args:
+      suffix(str): suffix if the file name to be found
+      path(str): path of the file system
+
+    Returns:
+       a list of paths
+    """
+    return None

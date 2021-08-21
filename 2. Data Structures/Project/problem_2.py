038b1058db2305ce22d4e3c1b1210191e9b291da
hlin <henrylin2008@yahoo.com>
2021-08-21 08:39:28 -0700

    find files solution

--- 2. Data Structures/Project/problem_2.py
+++ 2. Data Structures/Project/problem_2.py
@@ -35,28 +35,29 @@
 # Note: os.walk() is a handy Python method which can achieve this task very easily. However, for this problem you are
 # not allowed to use os.walk().
 #
-# Here is some code for the function to get you started:
-
 import os
-
-# Let us print the files in the directory in which you are running this script
-# print(os.listdir("."))
-
 
 def find_files(suffix, path):
     """
     Find all files beneath path with file name suffix.
-
     Note that a path may contain further subdirectories
     and those subdirectories may also contain further subdirectories.
-
     There are no limit to the depth of the subdirectories can be.
-
     Args:
       suffix(str): suffix if the file name to be found
       path(str): path of the file system
-
     Returns:
        a list of paths
     """
-    return None
+
+    paths = []
+
+    for item in os.listdir(path):
+        path_item = os.path.join(path, item)
+
+        if os.path.isfile(path_item) and path_item.endswith(suffix):
+            paths.append(path_item)
+        elif os.path.isdir(path_item):
+            paths += find_files(suffix, path_item)
+
+    return paths

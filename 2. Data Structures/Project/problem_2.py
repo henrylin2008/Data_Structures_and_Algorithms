2bd62d0d604cbc4d0714352696ad2f70d24b8343
hlin <henrylin2008@yahoo.com>
2021-08-21 09:51:07 -0700

    edge case and test cases

--- 2. Data Structures/Project/problem_2.py
+++ 2. Data Structures/Project/problem_2.py
@@ -52,12 +52,49 @@ def find_files(suffix, path):
 
     paths = []
 
-    for item in os.listdir(path):
-        path_item = os.path.join(path, item)
+    if os.path.exists(path):
+        for item in os.listdir(path):
+            path_items = os.path.join(path, item)
 
-        if os.path.isfile(path_item) and path_item.endswith(suffix):
-            paths.append(path_item)
-        elif os.path.isdir(path_item):
-            paths += find_files(suffix, path_item)
+            if os.path.isfile(path_items) and path_items.endswith(suffix):  # base case
+                paths.append(path_items)
+            elif os.path.isdir(path_items):
+                paths += find_files(suffix, path_items)      # recursive call
+    else:  # edge case if path does not exist
+        return "Path does not exist"
 
     return paths
+
+
+# print files in current directory
+# print(os.listdir("."))
+
+# Test case if the file exist
+print(os.path.isfile("testdir/subdir1/a.h"))  # return True if the file exists
+
+# Test if the file ending with .py
+print("./test.py".endswith(".py"))    # return True if the file ending with .py extension
+
+# test if any file ending with .h in a given directory
+test1 = find_files(".h", "testdir/subdir5/")
+print(test1)     # return a list of files that ending with .h extension in the given directory
+
+# test edge case with non existing file and directory
+test2 = find_files(".non", "nonexist")
+print(test2)    # return "Path does not exist" if no such path
+
+# edge case non-exist file in the subdirectory
+test3 = find_files(".h", "testdir/nonexist")
+print(test3)    # return "Path does not exist" if no such path
+
+# test to get file/s ending with .h in the given directory and the subdirectories
+test4 = find_files(".h", "testdir")
+print(test4)  # prints files with .h extension
+
+# test to get non-existing file/s ending with .text in the current directory and the subdirectories
+test5 = find_files(".txt", "testdir")
+print(test5)  # prints a list of files ending with .txt extension in current directory/subdirectories
+
+# test to get file/s ending with .c in the given directory and the subdirectories
+test6 = find_files(".c", "testdir")
+print(test6)  # prints files with .c extension

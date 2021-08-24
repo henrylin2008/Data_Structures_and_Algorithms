7dea6cca11d5559775816a05f823dd1860cf40a3
hlin <henrylin2008@yahoo.com>
2021-08-23 23:55:19 -0700

    Blockchain

--- 2. Data Structures/Project/problem_5.py
+++ 2. Data Structures/Project/problem_5.py
@@ -7,34 +7,55 @@
 # Use your knowledge of linked lists and hashing to create a blockchain implementation.
 #
 # We can break the blockchain down into three main parts.
-#
-# First is the information hash:
+# #
 import hashlib
+import datetime
 
-
-def calc_hash(self):
-    sha = hashlib.sha256()
-
-    hash_str = "We are going to encode this string of data!".encode('utf-8')
-
-    sha.update(hash_str)
-
-    return sha.hexdigest()
-
-
-# We do this for the information we want to store in the block chain such as transaction time, data, and information
-# like the previous chain.
-#
-# The next main component is the block on the blockchain:
+# the block on the blockchain:
 class Block:
-
     def __init__(self, timestamp, data, previous_hash):
         self.timestamp = timestamp
         self.data = data
         self.previous_hash = previous_hash
         self.hash = self.calc_hash()
 
-# Above is an example of attributes you could find in a Block class.
-#
-# Finally you need to link all of this together in a block chain, which you will be doing by implementing it in a
-# linked list. All of this will help you build up to a simple but full blockchain implementation!
+    def calc_hash(self):
+        sha = hashlib.sha256()
+        hash_str = str(self.data).encode('utf-8')
+        sha.update(hash_str)
+        return sha.hexdigest()
+
+    def __str__(self):
+        return (f'Timestamp: {self.timestamp} \n '
+                f'Data: {self.data} \n'
+                f'Hash: {self.hash} \n'
+                f'Previous Hash: {self.previous_hash} ')
+
+def get_utc_time():
+    return datetime.datetime.now(datetime.timezone.utc).strftime("%m-%d-%y %H:%M:%S")
+
+class Node:
+    def __init__(self, node):
+        self.node = node
+        self.next = None
+
+class BlockChain:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_block(self, data):
+        if self.head is None:
+            timestamp = get_utc_time()
+            data = data
+            block = Block(timestamp, data, 0)
+            node = Node(block)
+            self.head = node
+            self.tail = node
+        else:
+            timestamp = get_utc_time()
+            data = data
+            block = Block(timestamp, data, self.tail.node.hash)
+            node = Node(block)
+            self.tail.next = node
+            self.tail = node

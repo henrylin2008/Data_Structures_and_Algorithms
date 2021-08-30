da5dd543c87b9c9b843f260691755c9d599f4e77
hlin <henrylin2008@yahoo.com>
2021-08-29 23:24:01 -0700

    Binary Search - First and last indexes

---
+++ 3. Basic Algorithms/Basic Algorithms/bs_first_and_last_indexes.py
@@ -0,0 +1,9 @@
+# Binary Search: first and last indexes
+# Problem statement
+# Given a sorted array that may have duplicate values, use binary search to find the first and last indexes of a
+# given value.
+#
+# For example, if you have the array [0, 1, 2, 2, 3, 3, 3, 4, 5, 6] and the given value is 3, the answer will be [4,
+# 6] (because the value 3 occurs first at index 4 and last at index 6 in the array).
+#
+# The expected complexity of the problem is  𝑂(𝑙𝑜𝑔(𝑛)) .

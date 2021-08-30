ad3cedc810a90319f0c74823313d16a7148b0d8c
hlin <henrylin2008@yahoo.com>
2021-08-29 23:24:38 -0700

    Binary Search - First and last indexes solution

--- 3. Basic Algorithms/Basic Algorithms/bs_first_and_last_indexes.py
+++ 3. Basic Algorithms/Basic Algorithms/bs_first_and_last_indexes.py
@@ -7,3 +7,52 @@
 # 6] (because the value 3 occurs first at index 4 and last at index 6 in the array).
 #
 # The expected complexity of the problem is  𝑂(𝑙𝑜𝑔(𝑛)) .
+
+def first_and_last_index(arr, number):
+    # search first occurrence
+    first_index = find_start_index(arr, number, 0, len(arr) - 1)
+
+    # search last occurrence
+    last_index = find_end_index(arr, number, 0, len(arr) - 1)
+    return [first_index, last_index]
+
+
+def find_start_index(arr, number, start_index, end_index):
+    # binary search solution to search for the first index of the array
+    if start_index > end_index:
+        return -1
+
+    mid_index = start_index + (end_index - start_index) // 2
+
+    if arr[mid_index] == number:
+        current_start_pos = find_start_index(arr, number, start_index, mid_index - 1)
+        if current_start_pos != -1:
+            start_pos = current_start_pos
+        else:
+            start_pos = mid_index
+        return start_pos
+
+    elif arr[mid_index] < number:
+        return find_start_index(arr, number, mid_index + 1, end_index)
+    else:
+        return find_start_index(arr, number, start_index, mid_index - 1)
+
+
+def find_end_index(arr, number, start_index, end_index):
+    # binary search solution to search for the last index of the array
+    if start_index > end_index:
+        return -1
+
+    mid_index = start_index + (end_index - start_index) // 2
+
+    if arr[mid_index] == number:
+        current_end_pos = find_end_index(arr, number, mid_index + 1, end_index)
+        if current_end_pos != -1:
+            end_pos = current_end_pos
+        else:
+            end_pos = mid_index
+        return end_pos
+    elif arr[mid_index] < number:
+        return find_end_index(arr, number, mid_index + 1, end_index)
+    else:
+        return find_end_index(arr, number, start_index, mid_index - 1)

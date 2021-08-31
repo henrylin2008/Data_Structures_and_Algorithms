74b89891d7540b4a95267c0c7cde47147c6cc86d
hlin <henrylin2008@yahoo.com>
2021-08-30 22:34:12 -0700

    basic_trie and is_wood

--- 3. Basic Algorithms/Basic Algorithms/tries.py
+++ 3. Basic Algorithms/Basic Algorithms/tries.py
@@ -11,3 +11,53 @@
 #
 # Basic Trie
 # Let's look at a basic Trie with the following words: "a", "add", and "hi"
+
+basic_trie = {
+    # a and add word
+    'a': {
+        'd': {
+            'd': {'word_end': True},
+            'word_end': False},
+        'word_end': True},
+    # hi word
+    'h': {
+        'i': {'word_end': True},
+        'word_end': False}}
+
+
+print('Is "a"   a word: {}'.format(basic_trie['a']['word_end']))        # True
+print('Is "ad"  a word: {}'.format(basic_trie['a']['d']['word_end']))   # False
+print('Is "add" a word: {}'.format(basic_trie['a']['d']['d']['word_end']))  # True
+
+# You can lookup a word by checking if word_end is True after traversing all the characters in the word. Let's look
+# at the word "hi". The first letter is "h", so you would call basic_trie['h']. The second letter is "i",
+# so you would call basic_trie['h']['i']. Since there's no more letters left, you would see if this is a valid word
+# by getting the value of word_end. Now you have basic_trie['h']['i']['word_end'] with True or False if the word
+# exists.
+
+# In basic_trie, words "a" and "add" overlapp. This is where a Trie saves memory. Instead of having "a" and "add" in
+# different cells, their characters treated like nodes in a tree. Let's see how we would check if a word exists in
+# basic_trie.
+
+def is_word(word):
+    """
+    Look for the word in `basic_trie`
+    """
+    current_node = basic_trie
+
+    for char in word:
+        if char not in current_node:
+            return False
+
+        current_node = current_node[char]
+
+    return current_node['word_end']
+
+
+# Test words
+test_words = ['ap', 'add']
+for word in test_words:
+    if is_word(word):
+        print('"{}" is a word.'.format(word))
+    else:
+        print('"{}" is not a word.'.format(word))

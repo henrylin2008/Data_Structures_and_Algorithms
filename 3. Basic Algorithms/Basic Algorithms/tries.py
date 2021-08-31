6cdac2692d38499a539e65eaf45dd1f671712d4d
hlin <henrylin2008@yahoo.com>
2021-08-30 22:40:59 -0700

    Trie using DeafultDict test cases

--- 3. Basic Algorithms/Basic Algorithms/tries.py
+++ 3. Basic Algorithms/Basic Algorithms/tries.py
@@ -175,3 +175,20 @@ class Trie(object):
 
         return current_node.is_word
 
+
+# Add words
+valid_words = ['the', 'a', 'there', 'answer', 'any', 'by', 'bye', 'their']
+word_trie = Trie()
+for valid_word in valid_words:
+    word_trie.add(valid_word)
+
+# Tests
+assert word_trie.exists('the')
+assert word_trie.exists('any')
+assert not word_trie.exists('these')
+assert not word_trie.exists('zzz')
+print('All tests passed!')      # All tests passed!
+
+# The Trie data structure is part of the family of Tree data structures. It shines when dealing with sequence data,
+# whether it's characters, words, or network nodes. When working on a problem with sequence data, ask yourself if a Trie
+# is right for the job.

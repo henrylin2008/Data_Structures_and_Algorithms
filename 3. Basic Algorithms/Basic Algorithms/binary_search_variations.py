e03b4219a5708d44760d0c3e13eaf44bd5419dfa
hlin <henrylin2008@yahoo.com>
2021-09-05 23:32:02 -0700

    Binary Search Variation

--- 3. Basic Algorithms/Basic Algorithms/binary_search_variations.py
+++ 3. Basic Algorithms/Basic Algorithms/binary_search_variations.py
@@ -1,4 +1,4 @@
-# # Variations on Binary Search
+# Variations on Binary Search
 #
 # Now that you've gone through the work of building a binary search function, let's take some time to try out a few
 # exercises that are variations (or extensions) of binary search. We'll provide the function for you to start:
@@ -15,6 +15,7 @@ def recursive_binary_search(target, source, left=0):
 
 
 # Find First
+
 # The binary search function is guaranteed to return an index for the element you're looking for in an
 # array, but what if the element appears more than once?
 #
@@ -25,3 +26,30 @@ def recursive_binary_search(target, source, left=0):
 # Let's find the number 7:
 multiple = [1, 3, 5, 7, 7, 7, 8, 11, 12]
 recursive_binary_search(7, multiple)  # 4
+
+
+# Hmm...
+# Looks like we got the index 4, which is correct, but what if we wanted to find the first occurrence of an element,
+# rather than just any occurrence?
+#
+# Write a new function: find_first() that uses binary_search as a starting point.
+#
+# Hint: You shouldn't need to modify binary_search() at all.
+def find_first(target, source):
+    index = recursive_binary_search(target, source)
+    if not index:
+        return None
+    while source[index] == target:
+        if index == 0:
+            return 0
+        if source[index - 1] == target:
+            index -= 1
+        else:
+            return index
+
+
+multiple1 = [1, 3, 5, 7, 7, 7, 8, 11, 12, 13, 14, 15]
+print(find_first(7, multiple1))  # Should return 3
+print(find_first(9, multiple1))  # Should return None
+
+

9332c699873730e86ce916f07fdf32550c96607b
hlin <henrylin2008@yahoo.com>
2021-09-16 23:39:42 -0700

    Square Root of an integer

--- 3. Basic Algorithms/Problems/problem_1.py
+++ 3. Basic Algorithms/Problems/problem_1.py
@@ -20,11 +20,34 @@ def sqrt(number):
     Returns:
        int: Floored Square Root
     """
-    pass
+    if number < 0:
+        return None
+
+    if number == 0 or number == 1:
+        return number
+
+    return sqrt_recursive(number, 0, number)
 
 
+def sqrt_recursive(target, start, end):
+    if start >= end:
+        return start
+
+    mid = (start + end) // 2
+
+    if mid ** 2 == target:
+        return mid
+
+    elif mid ** 2 < target:
+        return sqrt_recursive(target, mid + 1, end)
+
+    return sqrt_recursive(target, start, mid - 1)
+
+
+# Test cases
 print("Pass" if (3 == sqrt(9)) else "Fail")
 print("Pass" if (0 == sqrt(0)) else "Fail")
 print("Pass" if (4 == sqrt(16)) else "Fail")
 print("Pass" if (1 == sqrt(1)) else "Fail")
 print("Pass" if (5 == sqrt(27)) else "Fail")
+print("Pass" if (sqrt(-1) is None) else "Fail")

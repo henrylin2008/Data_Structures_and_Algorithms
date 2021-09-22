067243c6ac1d871e7dc1a2ee9aee516ff09838cb
hlin <henrylin2008@yahoo.com>
2021-09-22 08:28:43 -0700

    Dutch National Flag

--- 3. Basic Algorithms/Problems/problem_4.py
+++ 3. Basic Algorithms/Problems/problem_4.py
@@ -34,7 +34,7 @@ def sort_012(input_list):
     return input_list
 
 
-def t_fn(test_case):
+def test_function(test_case):
     sorted_array = sort_012(test_case)
     print(sorted_array, end=': ')
     if sorted_array == sorted(test_case):
@@ -44,6 +44,21 @@ def t_fn(test_case):
 
 
 print("Test cases:")
-t_fn([0, 0, 2, 2, 2, 0, 0, 1, 2, 0, 2])
-t_fn([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
-t_fn([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
+test_function([0, 0, 2, 2, 2, 0, 0, 1, 2, 0, 2])
+test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
+test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
+
+print("Edge cases:")
+test_function([])
+test_function([0])
+test_function([0, 0, 0])
+
+# Output:
+# Test cases:
+# [0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2]: Pass
+# [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2]: Pass
+# [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2]: Pass
+# Edge cases:
+# []: Pass
+# [0]: Pass
+# [0, 0, 0]: Pass

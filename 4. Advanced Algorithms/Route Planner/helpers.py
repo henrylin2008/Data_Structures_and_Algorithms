27893deb0c28062008fc64528568230669d2e56f
hlin <henrylin2008@yahoo.com>
2021-11-21 14:18:24 -0800

    route planner

--- 4. Advanced Algorithms/Route Planner/helpers.py
+++ 4. Advanced Algorithms/Route Planner/helpers.py
@@ -7,29 +7,31 @@ from plotly.offline import init_notebook_mode, plot, iplot
 init_notebook_mode(connected=True)
 
 class Map:
-	def __init__(self, G):
-		self._graph = G
-		self.intersections = nx.get_node_attributes(G, "pos")
-		self.roads = [list(G[node]) for node in G.nodes()]
+    def __init__(self, G):
+        self._graph = G
+        self.intersections = nx.get_node_attributes(G, "pos")
+        self.roads = [list(G[node]) for node in G.nodes()]
 
-	def save(self, filename):
-		with open(filename, 'wb') as f:
-			pickle.dump(self._graph, f)
+    def save(self, filename):
+        with open(filename, 'wb') as f:
+            pickle.dump(self._graph, f)
+
 
 def load_map(name):
-	with open(name, 'rb') as f:
-		G = pickle.load(f)
-	return Map(G)
+    with open(name, 'rb') as f:
+        G = pickle.load(f)
+    return Map(G)
+
 
 def show_map(M, start=None, goal=None, path=None):
     G = M._graph
     pos = nx.get_node_attributes(G, 'pos')
     edge_trace = Scatter(
-    x=[],
-    y=[],
-    line=Line(width=0.5,color='#888'),
-    hoverinfo='none',
-    mode='lines')
+        x=[],
+        y=[],
+        line=Line(width=0.5, color='#888'),
+        hoverinfo='none',
+        mode='lines')
 
     for edge in G.edges():
         x0, y0 = G.node[edge[0]]['pos']
@@ -79,13 +81,13 @@ def show_map(M, start=None, goal=None, path=None):
 
     fig = Figure(data=Data([edge_trace, node_trace]),
                  layout=Layout(
-                    title='<br>Network graph made with Python',
-                    titlefont=dict(size=16),
-                    showlegend=False,
-                    hovermode='closest',
-                    margin=dict(b=20,l=5,r=5,t=40),
-                   
-                    xaxis=XAxis(showgrid=False, zeroline=False, showticklabels=False),
-                    yaxis=YAxis(showgrid=False, zeroline=False, showticklabels=False)))
+                     title='<br>Network graph made with Python',
+                     titlefont=dict(size=16),
+                     showlegend=False,
+                     hovermode='closest',
+                     margin=dict(b=20, l=5, r=5, t=40),
+
+                     xaxis=XAxis(showgrid=False, zeroline=False, showticklabels=False),
+                     yaxis=YAxis(showgrid=False, zeroline=False, showticklabels=False)))
 
     iplot(fig)
